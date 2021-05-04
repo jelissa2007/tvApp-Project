e7a23d5ca241db9b8a95672feebe9ee22bdcc79d
myrriame <myrriametiffany@gmail.com>
2021-05-03 20:18:22 -0400

    added buttons to each show poster

--- tv.js
+++ tv.js
@@ -1,15 +1,27 @@
+const posterDiv = document.getElementById('poster_container')
+
+
 const addPoster = (data) => {
     console.log(data)
     for (let i = 0; i < 10; i++) {
         const poster = document.createElement('img')
+        const bttn = document.createElement('button')
 
         if (data[i].show.image != null) {
-
+            bttn.innerHTML = "Add to List";
+            bttn.className = 'bttn'
+            posterDiv.appendChild(bttn)
             poster.src = data[i].show.image.medium
             console.log(poster)
-            document.body.appendChild(poster)
+
+            posterDiv.appendChild(poster)
+            poster.className = `pic${[i]}`
+
+
+
         }
     }
+
 }
 
 function doSearch(event) {
@@ -28,7 +40,4 @@ function doSearch(event) {
 document.getElementById('netflix').addEventListener('click', function (e) {
     window.open('http://www.netflix.com', '_blank');
 });
-
-
-
 
