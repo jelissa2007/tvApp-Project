a39ed3e0957f25fa38ce1611c76766e90bdf1cbe
csolis1 <celida.solis@outlook.com>
2021-05-03 20:59:35 -0400

    added genres, streaming platform, title and summary

--- tv.js
+++ tv.js
@@ -1,16 +1,31 @@
 const addPoster = (data) => {
     console.log(data)
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < data.length; i++) { //changed from 10 to data.length to avoid error
         const poster = document.createElement('img')
+        const title = document.createElement('h3')
+        const genres = document.createElement('h5')
+        const summary = document.createElement('p')
+        const streamingPlatform = document.createElement('h5')
 
-        if (data[i].show.image != null) {
+        if (data[i].show.image != null && data[i].show.webChannel != null) { // will not show shows that have no poster and that don't have a streaming channelgit
 
             poster.src = data[i].show.image.medium
+            title.innerHTML = data[i].show.name
+            genres.innerHTML = data[i].show.genres
+            summary.innerHTML = data[i].show.summary
+            streamingPlatform.innerHTML = data[i].show.webChannel.name
             console.log(poster)
             document.body.appendChild(poster)
+            document.body.appendChild(title)
+            document.body.appendChild(genres)
+            document.body.appendChild(summary)
+            document.body.appendChild(streamingPlatform)
+
         }
     }
 }
+
+
 
 function doSearch(event) {
     event.preventDefault()
@@ -23,6 +38,18 @@ function doSearch(event) {
         .then(reply => addPoster(reply))
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
